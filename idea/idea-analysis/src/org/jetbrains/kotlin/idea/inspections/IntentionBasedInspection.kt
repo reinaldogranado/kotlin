77a3772729213b1d055c1967f8c862441e7041ff
Valentin Kipyatkov <valentin@jetbrains.com>
2016-03-08 18:12:23 +0100

    Initial implementation of loop-to-call-chain intention

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/inspections/IntentionBasedInspection.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/inspections/IntentionBasedInspection.kt
@@ -40,8 +40,11 @@ abstract class IntentionBasedInspection<TElement : KtElement>(
         protected val elementType: Class<TElement>
 ) : AbstractKotlinInspection() {
 
-    constructor(intention: SelfTargetingRangeIntention<TElement>, additionalChecker: (TElement) -> Boolean = { true })
-    : this(listOf(IntentionData(intention, additionalChecker)), null, intention.elementType)
+    constructor(
+            intention: SelfTargetingRangeIntention<TElement>,
+            additionalChecker: (TElement) -> Boolean = { true },
+            problemText: String? = null
+    ) : this(listOf(IntentionData(intention, additionalChecker)), problemText, intention.elementType)
 
     data class IntentionData<TElement : KtElement>(
             val intention: SelfTargetingRangeIntention<TElement>,
