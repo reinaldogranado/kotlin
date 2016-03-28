c1c92aab155a02e8656f20f094f7e207c2b0fe52
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2016-03-28 14:01:32 +0300

    fixup 5851037

--- js/js.translator/testData/js-optimizer/temporary-assignment/declaration.original.js
+++ js/js.translator/testData/js-optimizer/temporary-assignment/declaration.original.js
@@ -11,10 +11,10 @@ function test(n) {
 }
 
 function box() {
-    var result = test(20)
+    var result = test(20);
     if (result != 20) return "fail1: " + result;
 
-    result = test(-20)
+    result = test(-20);
     if (result != 20) return "fail2: " + result;
 
     return "OK"
